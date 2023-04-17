23d7aab1c5415f75d3de73188b3451fa160696aa
Nigel Munro <munro98@gmail.com>
2023-04-17 14:55:30 +1200

    edit script

--- render_build.sh
+++ render_build.sh
@@ -3,10 +3,10 @@ npm install
 #install web server
 npm install serve
 #install react server dependencies
-cd /home/ec2-user/app/client
+cd ./client
 #install node modules
 npm install -f
 #build production react application
 npm run build
-cd /home/ec2-user/app
+cd ../
 export MY_SECRET="cool beans"
